73ddf77453537bf6a96e73d2a785712a5af34be7
mattkrebs1974 <mattkrebsemail@gmail.com>
2020-08-22 17:37:39 -0400

    fix connection

--- config/application.php
+++ config/application.php
@@ -103,9 +103,9 @@ define('WP_CONTENT_URL', WP_HOME . CONTENT_DIR);
 /**
  * DB settings
  */
-define('DB_NAME', 'root' );
-define('DB_USER', 'root' );
-define('DB_PASSWORD', 'root');
+define('DB_NAME', env('DB_NAME'));
+define('DB_USER', env('DB_USER'));
+define('DB_PASSWORD', env('DB_PASSWORD'));
 define('DB_HOST', env('DB_HOST') ?: 'localhost');
 define('DB_CHARSET', 'utf8mb4');
 define('DB_COLLATE', '');
